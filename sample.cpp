f166e56c3df58e02728019612e5c1e9e3a99b2b7
minddrop <ryooooooo1@gmail.com>
2019-06-22 22:43:46 +0900

    my first rendering

--- sample.cpp
+++ sample.cpp
@@ -1,9 +1,17 @@
+#include <fstream>
 #include <iostream>
 
 int main() {
+  const char *fileName = "./out.ppm";
+  std::ofstream ofs(fileName);
+  if (!ofs) {
+    std::cout << "File is broken." << std::endl;
+    return 0;
+  }
+
   int nx = 200;
   int ny = 100;
-  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  ofs << "P3\n" << nx << " " << ny << "\n255\n";
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       float r = float(i) / float(nx);
@@ -12,7 +20,9 @@ int main() {
       int ir = int(255.99 * r);
       int ig = int(255.99 * g);
       int ib = int(255.99 * b);
-      std::cout << ir << " " << ig << " " << ib << "\n";
+      ofs << ir << " " << ig << " " << ib << "\n";
     }
   }
+  ofs.close();
+  return 0;
 }
