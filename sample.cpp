efb4d954dd82c58256a4b77dcc48fd2adde37d17
minddrop <ryooooooo1@gmail.com>
2019-06-23 22:17:56 +0900

    finished chapter2

--- sample.cpp
+++ sample.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include "vec3.h"
 
 int main() {
   const char *fileName = "./out.ppm";
@@ -12,14 +13,16 @@ int main() {
   int nx = 200;
   int ny = 100;
   ofs << "P3\n" << nx << " " << ny << "\n255\n";
+
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
+      vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
       float r = float(i) / float(nx);
       float g = float(j) / float(ny);
       float b = 0.2;
-      int ir = int(255.99 * r);
-      int ig = int(255.99 * g);
-      int ib = int(255.99 * b);
+      int ir = int(255.99 * col[0]);
+      int ig = int(255.99 * col[1]);
+      int ib = int(255.99 * col[2]);
       ofs << ir << " " << ig << " " << ib << "\n";
     }
   }
