587c635a899daa1a0860a2b7c1cdd31439cb76aa
minddrop <ryooooooo1@gmail.com>
2019-06-28 22:11:03 +0900

    improved readability

--- src/normalsample.cpp
+++ src/normalsample.cpp
@@ -17,9 +17,11 @@ float hit_sphere(const vec3& center, float radius, const ray& r) {
 }
 
 vec3 color(const ray& r) {
-  float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+  vec3 center(0, 0, -1);
+  float radius = 0.5;
+  float t = hit_sphere(center, radius, r);
   if (t > 0.0) {
-    vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+    vec3 N = unit_vector(r.point_at_parameter(t) - center);
     return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
   }
   vec3 unit_direction = unit_vector(r.direction());
