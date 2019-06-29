268d3f21e5fe2f8ddb881375c8b9a9e31e571bb2
minddrop <ryooooooo1@gmail.com>
2019-06-29 23:16:08 +0900

    fixed vec3.h

--- src/vec3.h
+++ src/vec3.h
@@ -77,7 +77,7 @@ inline vec3 operator*(float t, const vec3 &v) {
   return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator/(vec3 &v, float t) {
+inline vec3 operator/(const vec3 &v, float t) {
   return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
 }
 
@@ -120,6 +120,21 @@ inline vec3 &vec3::operator/=(const vec3 &v) {
   e[0] /= v.e[0];
   e[1] /= v.e[1];
   e[2] /= v.e[2];
+  return *this;
+}
+
+inline vec3 &vec3::operator*=(const float t) {
+  e[0] *= t;
+  e[1] *= t;
+  e[2] *= t;
+  return *this;
+}
+
+inline vec3 &vec3::operator/=(const float t) {
+  float k = 1.0 / t;
+  e[0] *= k;
+  e[1] *= k;
+  e[2] *= k;
   return *this;
 }
 
