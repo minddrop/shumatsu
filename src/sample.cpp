eb0924a61a1eb13e1101b27f89627cafb0e804a3
minddrop <ryooooooo1@gmail.com>
2019-08-24 21:24:19 +0900

    updated early sample

--- src/sample.cpp
+++ src/sample.cpp
@@ -17,9 +17,6 @@ int main() {
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-      float r = float(i) / float(nx);
-      float g = float(j) / float(ny);
-      float b = 0.2;
       int ir = int(255.99 * col[0]);
       int ig = int(255.99 * col[1]);
       int ib = int(255.99 * col[2]);
